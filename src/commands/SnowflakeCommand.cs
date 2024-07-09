404b20fed5d6b6cab8433a5379b74df636ae4857
Steven <github@steven-h.net>
2024-07-09 19:06:05 -0400

    This worked.

--- src/commands/SnowflakeCommand.cs
+++ src/commands/SnowflakeCommand.cs
@@ -1,3 +1,4 @@
+// SnowflakeCommand.cs in broken state
 using System.ComponentModel;
 using System.Text.RegularExpressions;
 using DSharpPlus.Commands;
@@ -8,16 +9,6 @@ using Nixill.Utils;
 using NodaTime;
 
 namespace Nixill.Discord.ShadowBot;
-
-public static class SnowflakeFunc
-{
-  internal static readonly Instant Epoch = Instant.FromUtc(2015, 1, 1, 0, 0);
-
-  internal static ulong Milliseconds(ulong snowflake) => snowflake >> 22;
-  internal static Instant InstantOf(ulong snowflake) => Epoch + Duration.FromMilliseconds(Milliseconds(snowflake));
-  internal static string Description(ulong stamp) => Description(stamp, InstantOf(stamp));
-  internal static string Description(ulong stamp, Instant time) => $"{stamp}: <t:{time.ToUnixTimeSeconds()}:d> <t:{time.ToUnixTimeSeconds()}:T> (+ `{time.ToUnixTimeMilliseconds() % 1000:000}` ms)";
-}
 
 [InteractionAllowedContexts(
   DiscordInteractionContextType.Guild,
@@ -32,6 +23,8 @@ public static class SnowflakeFunc
 public static class SnowflakeCommand
 {
   static Regex SnowflakeRegex = new(@"^https:\/\/discord\.com\/channels\/(@me|\d+)\/(\d+)(?:\/(\d+))$");
+
+  static readonly Instant Epoch = Instant.FromUtc(2015, 1, 1, 0, 0);
 
   [Command("to_time")]
   [Description("Get the timestamp of a snowflake or URL object.")]
@@ -48,7 +41,7 @@ public static class SnowflakeCommand
 
     foreach ((Instant time, string label, ulong stamp) in GetInstantForSnowflakes(input))
     {
-      builder.AddField(label, SnowflakeFunc.Description(stamp));
+      builder.AddField(label, Description(stamp));
     }
 
     if (builder.Fields.Count == 0) builder.AddField("Oops!", "There are no recognized snowflakes in your input.");
@@ -56,40 +49,7 @@ public static class SnowflakeCommand
     await ctx.RespondAsync(builder, ephemeral);
   }
 
-  static IEnumerable<(Instant Time, string Label, ulong Stamp)> GetInstantForSnowflakes(string input)
-  {
-    if (ulong.TryParse(input, out ulong stamp))
-    {
-      yield return (SnowflakeFunc.InstantOf(stamp), "Input", stamp);
-    }
-    else if (SnowflakeRegex.TryMatch(input, out Match mtc))
-    {
-      ulong val;
-
-      if (mtc.Groups[1].Success && ulong.TryParse(mtc.Groups[1].Value, out val))
-        yield return (SnowflakeFunc.InstantOf(val), "Server", val);
-
-      if (mtc.Groups[2].Success && ulong.TryParse(mtc.Groups[2].Value, out val))
-        yield return (SnowflakeFunc.InstantOf(val), "Channel", val);
-
-      if (mtc.Groups[3].Success && ulong.TryParse(mtc.Groups[3].Value, out val))
-        yield return (SnowflakeFunc.InstantOf(val), "Message", val);
-    }
-  }
-}
-
-[TopLevelCommand]
-public static class SnowflakeMessageCommand
-{
-  [InteractionAllowedContexts(
-    DiscordInteractionContextType.Guild,
-    DiscordInteractionContextType.BotDM,
-    DiscordInteractionContextType.PrivateChannel
-  )]
-  [InteractionInstallType(
-    DiscordApplicationIntegrationType.UserInstall
-  )]
-  [Command("Snowflakes to Time")]
+  [Command("to Time")]
   [Description("Get the timestamps of a snowflake associated with a message.")]
   [SlashCommandTypes(DiscordApplicationCommandType.MessageContextMenu)]
   public static async Task SnowflakeToTimeMessageCommand(SlashCommandContext ctx,
@@ -102,19 +62,50 @@ public static class SnowflakeMessageCommand
       Color = new DiscordColor("b42b42")
     };
 
-    embed.AddField("Message", SnowflakeFunc.Description(msg.Id), true);
+    embed.AddField("Message", Description(msg.Id), true);
 
     ulong? id = msg.Author?.Id;
-    if (id.HasValue) embed.AddField("Author", SnowflakeFunc.Description(id.Value), true);
+    if (id.HasValue) embed.AddField("Author", Description(id.Value), true);
 
-    embed.AddField("Channel", SnowflakeFunc.Description(msg.ChannelId), true);
+    embed.AddField("Channel", Description(msg.ChannelId), true);
 
     id = msg.Channel?.GuildId;
-    if (id.HasValue) embed.AddField("Server", SnowflakeFunc.Description(id.Value), true);
+    if (id.HasValue) embed.AddField("Server", Description(id.Value), true);
 
     id = msg.WebhookId;
-    if (id.HasValue) embed.AddField("Webhook", SnowflakeFunc.Description(id.Value), true);
+    if (id.HasValue) embed.AddField("Webhook", Description(id.Value), true);
 
     await ctx.RespondAsync(embed, true);
   }
+
+  static IEnumerable<(Instant Time, string Label, ulong Stamp)> GetInstantForSnowflakes(string input)
+  {
+    if (ulong.TryParse(input, out ulong stamp))
+    {
+      yield return (InstantOf(stamp), "Input", stamp);
+    }
+    else if (SnowflakeRegex.TryMatch(input, out Match mtc))
+    {
+      ulong val;
+
+      if (mtc.Groups[1].Success && ulong.TryParse(mtc.Groups[1].Value, out val))
+        yield return (InstantOf(val), "Server", val);
+
+      if (mtc.Groups[2].Success && ulong.TryParse(mtc.Groups[2].Value, out val))
+        yield return (InstantOf(val), "Channel", val);
+
+      if (mtc.Groups[3].Success && ulong.TryParse(mtc.Groups[3].Value, out val))
+        yield return (InstantOf(val), "Message", val);
+    }
+  }
+
+  static ulong Milliseconds(ulong snowflake) => snowflake >> 22;
+  static Instant InstantOf(ulong snowflake) => Epoch + Duration.FromMilliseconds(Milliseconds(snowflake));
+  static string Description(ulong stamp) => Description(stamp, InstantOf(stamp));
+  static string Description(ulong stamp, Instant time) => $"{stamp}: <t:{time.ToUnixTimeSeconds()}:f> (+ `{time.ToUnixTimeMilliseconds() % 1000}` ms)";
+}
+
+public static class SnowflakeMessageCommand
+{
+
 }
